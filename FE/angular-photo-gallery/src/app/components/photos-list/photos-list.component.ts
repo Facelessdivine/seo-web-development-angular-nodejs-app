bd9302fb1ece11f002995a2737764b9f4112a91f
facelessdivine <facelessdivine@gmail.com>
2022-02-09 21:49:59 -0700

    Works DOM function with a event button

--- FE/angular-photo-gallery/src/app/components/photos-list/photos-list.component.ts
+++ FE/angular-photo-gallery/src/app/components/photos-list/photos-list.component.ts
@@ -3,7 +3,6 @@ import { Router } from "@angular/router";
 
 import { PhotoService } from "../../services/photo.service";
 import { Photo } from "../../interfaces/Photo";
-
 @Component({
   selector: "app-photos-list",
   templateUrl: "./photos-list.component.html",
@@ -11,8 +10,9 @@ import { Photo } from "../../interfaces/Photo";
 })
 export class PhotosListComponent implements OnInit {
   photos: Photo[] = [];
+  isEnabled: boolean = true;
 
-  constructor(private photoService: PhotoService, private router: Router) {}
+  constructor(private photoService: PhotoService, private router: Router) { }
 
   ngOnInit() {
     this.photoService.getPhotos().subscribe(
@@ -26,8 +26,22 @@ export class PhotosListComponent implements OnInit {
   selectedCard(id: string) {
     this.router.navigate(["/photos", id]);
   }
-  changeStyle = function () {
-   
-    document.getElementById("clickToChange").style.backgroundColor = "red";
+
+  changeStyle() {
+    let setProp = '--td-background-color'
+    const tds = document.querySelectorAll('div');
+    if (this.isEnabled) {
+      tds.forEach((td) => {
+        td.style.setProperty(setProp, '#ffff00');
+      })
+      this.isEnabled = false
+    }
+    else {
+      tds.forEach((td) => {
+        td.style.setProperty(setProp, '#333');
+        this.isEnabled = true
+      })
+    }
+    console.log(this.isEnabled)
   };
 }
